0a28a284ba03ac1af932159eb6cc378a9f8f5351
ridaarif98 <ridaarif16@gmail.com>
2021-10-04 21:27:31 +0200

    Style rocket UI

--- src/components/MyRockets.js
+++ src/components/MyRockets.js
@@ -1,6 +1,7 @@
 import React,  { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadRockets } from '../redux/rockets/rockets';
+import '../stylesheets/rockets.css';
 
 function MyRockets() {
   const dispatch = useDispatch();
@@ -13,13 +14,18 @@ function MyRockets() {
   return (
     <div className="my-rockets">
       <h2>My Rockets</h2>
-      <div className="div">
+      <div className="rocket-div">
         <ul>
           {rockets.map((rocket) => (
-            <li>
-             <span> {rocket.id} {rocket.rocket_name}</span> 
+            <li key={rocket.id}>
+            <div class="img-rocket">
+            <img src={rocket.flickr_images} alt="rcoket"/>
+            </div>
+            <div className="rocket-detail">
+             <h2> {rocket.rocket_name}</h2> 
              <p>{rocket.description}</p>
-             <img src={rocket.flickr_images} alt="rcoket"/>
+             <button type="button" id={rocket.id}>Reserve Rocket</button>
+             </div>
             </li>
           ))}
         </ul>
