b77785136c428658b13b60365f70f3993a3d17fa
ridaarif98 <ridaarif16@gmail.com>
2021-10-05 13:23:49 +0200

    Fix some linter errors

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -8,14 +8,12 @@ export const loadRockets = async (dispatch) => {
   const rocketGet = await fetch('https://api.spacexdata.com/v3/rockets');
   const rocketList = await rocketGet.json();
   const rocketsData = [];
-  rocketList.map((rocket) =>
-    rocketsData.push({
+  rocketList.map((rocket) => rocketsData.push({
       id: rocket.id,
       rocket_name: rocket.rocket_name,
       description: rocket.description,
       flickr_images: rocket.flickr_images,
-    })
-  );
+  }));
   dispatch({
     type: FETCH_ROCKETS,
     payload: rocketsData,
