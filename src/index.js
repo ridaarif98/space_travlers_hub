f563321f9ce6f5ccb94500ac8d541f20bad9213e
Amine Smahi <md.amine.smahi@gmail.com>
2021-10-06 17:40:27 +0100

    Remove useless lines

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const initialState = [];
-export default initialState;
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
