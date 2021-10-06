84be6116846ed9c4d224d68fda39a6a317a54313
ridaarif98 <ridaarif16@gmail.com>
2021-10-06 11:39:05 +0200

    Use filter method to render a list of all reserved rockets in MyProfile

--- src/components/MyRockets.js
+++ src/components/MyRockets.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function MyRockets() {
+  const rockets = useSelector((state) => state.rocketsReducer);
+
   return (
     <div className="my-rockets">
       <h2>My Rockets</h2>
       <div className="div">
-        <p>Rockets ... </p>
+        <p> {rockets.filter((y) => y.reserved).map((y) => y.rocket_name)} </p>
       </div>
     </div>
   );
